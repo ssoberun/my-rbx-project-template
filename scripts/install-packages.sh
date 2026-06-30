77586e9c1e05e9c4e11990164cb4431b26348257
ssoberun <5744sam@gmail.com>
2026-06-30 10:31:38 +1000

    fix for wally package types

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -6,5 +6,5 @@ wally install
 
 # Patch the Wally package link modules to also export Luau type definitions.
 argon sourcemap default.project.json -o sourcemap.json
-~/nixdots/modules/features/packages/result/bin/wally-package-types --sourcemap sourcemap.json Packages/
-~/nixdots/modules/features/packages/result/bin/wally-package-types --sourcemap sourcemap.json ServerPackages/
+wally-package-types --sourcemap sourcemap.json Packages/
+wally-package-types --sourcemap sourcemap.json ServerPackages/
